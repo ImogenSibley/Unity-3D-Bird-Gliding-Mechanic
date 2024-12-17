f7f6fbb5720b063a522445626d279bb7f7228cc9
Imogen Sibley <Imogen-layla@hotmail.co.uk>
2024-12-17 13:38:52 +0000

    Removed unused lines of code

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -7,12 +7,6 @@ public class Gem : MonoBehaviour
     public GemData gemData; //reference to scriptable object containing gem data
     private AudioSource audioSource;
 
-    //void Start()
-    //{
-    //    // Get the AudioSource component attached to the GameObject
-    //    audioSource = GetComponent<AudioSource>();
-    //}
-
     private void OnTriggerEnter(Collider other) //method is called when another object collides with other object
     {
         if (other.CompareTag("Player")) //if player collides with gem
